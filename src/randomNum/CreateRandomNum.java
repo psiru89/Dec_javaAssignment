bef709463d7955a455d888ef46db914f109a5049
psiru89 <psiru89@gmail.com>
2023-06-15 17:34:36 -0600

    committed

---
+++ src/randomNum/CreateRandomNum.java
@@ -0,0 +1,18 @@
+package randomNum;
+
+import java.util.Scanner;
+
+public class CreateRandomNum {
+
+	public static void main(String[] args) {
+		
+		Scanner input= new Scanner(System.in);
+		int value1= input.nextInt();
+		
+        for(int value=1; value<=500;value++) {
+        	System.out.println(value);
+        
+        }
+	}
+
+}
